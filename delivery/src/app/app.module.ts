b622b47d4806bcbbd29af19b1f0ffb5b86264c5f
Vlad Kolesnikov <vkoles2611@gmail.com>
2020-07-30 22:08:25 +0300

    Some style fixes + deleted redundant packages.

--- delivery/src/app/app.module.ts
+++ delivery/src/app/app.module.ts
@@ -3,12 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './sandbox/components/hello/hello.component';
-import { HelloPage } from './sandbox/pages/hello/hello.page';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NumbersPage } from './sandbox/pages/numbers/numbers.page';
-import { CounterComponent } from './sandbox/components/counter/counter.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { DishesPage } from './routed/delivery/pages/dishes/dishes';
@@ -24,10 +20,6 @@ import { MainPage } from './routed/delivery/pages/main/main.page';
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    HelloPage,
-    NumbersPage,
-    CounterComponent,
     DishesPage,
     OrderPage,
     RestaurantsPage,
