b6e7765e6f0b16d1da2cf0b3bd6d6d0c3d46579d
stupak dasha <drsarya043@gmail.com>
2020-08-12 01:18:43 +0300

    Опять исправления

--- delivery/src/app/routed/delivery/components/dialogs/delete-order-dialog-admin/delete-order-dialog-admin.dialog.ts
+++ delivery/src/app/routed/delivery/components/dialogs/delete-order-dialog-admin/delete-order-dialog-admin.dialog.ts
@@ -25,7 +25,7 @@ export class DeleteOrderDialogAdminDialog implements OnInit {
   deleteOrder(): void {
     if (this.idOrder != undefined) {
 
-      this.orderApiService.deleteOrderById(this.idOrder).subscribe(
+      this.orderApiService.deleteOrderById(this.idOrder.toString()).subscribe(
         (res) => {
           // tslint:disable-next-line:no-console
           console.info(res);
