df448eda12e1e7ad2d4b00d9985b153707ac167a
stupak dasha <drsarya043@gmail.com>
2020-08-12 20:33:24 +0300

    Исправление наезжающих иконок, регулировка размеров фото в отображении позиции

--- delivery/src/app/routed/delivery/components/all-dishes/all-dishes.component.ts
+++ delivery/src/app/routed/delivery/components/all-dishes/all-dishes.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { DishFromBasketModel, DishModel } from '../../../../features/dishes/models/dish.model';
-import { HttpClient } from '@angular/common/http';
-import { MatDialog } from '@angular/material/dialog';
-import { AddDishToBasketDialogDialog } from '../dialogs/add-dish-to-basket-dialog/add-dish-to-basket-dialog.dialog';
-import { DataService } from '../../../../data.service';
-import { DishAlreadyInBasketDialogDialog } from '../dialogs/dish-already-in-basket-dialog/dish-already-in-basket-dialog.dialog';
-import { OrderAlreadyCreatedDialog } from '../dialogs/order-already-created/order-already-created.dialog';
-import { DishesApiService } from '../../../../features/dishes/services/dishes-api.service';
-import { OrdersApiService } from '../../../../features/orders/services/orders-api.service';
+import {Component, OnInit} from '@angular/core';
+import {DishFromBasketModel, DishModel} from '../../../../features/dishes/models/dish.model';
+import {HttpClient} from '@angular/common/http';
+import {MatDialog} from '@angular/material/dialog';
+import {AddDishToBasketDialogDialog} from '../dialogs/add-dish-to-basket-dialog/add-dish-to-basket-dialog.dialog';
+import {DataService} from '../../../../data.service';
+import {DishAlreadyInBasketDialogDialog} from '../dialogs/dish-already-in-basket-dialog/dish-already-in-basket-dialog.dialog';
+import {OrderAlreadyCreatedDialog} from '../dialogs/order-already-created/order-already-created.dialog';
+import {DishesApiService} from '../../../../features/dishes/services/dishes-api.service';
+import {OrdersApiService} from '../../../../features/orders/services/orders-api.service';
 
 @Component({
   selector: 'app-all-dishes',
@@ -31,7 +31,13 @@ export class AllDishesComponent implements OnInit {
     private readonly dataService: DataService,
     private readonly dishesApiService: DishesApiService,
     private readonly ordersApiService: OrdersApiService
-  ) {}
+  ) {
+  }
+
+  redacphoto(): void {
+
+
+  }
 
   ngOnInit(): void {
     this.dishesApiService.getAllDishes().subscribe((result) => {
